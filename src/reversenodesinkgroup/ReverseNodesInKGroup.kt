f0de1c9b869ae21418c2c9395d4f8c6366231e3c
Anton Kapelyushok <anton.kapelyushok@infobip.com>
2021-03-26 15:42:09 +0300

    reverse nodes in k group non recursive

--- src/reversenodesinkgroup/ReverseNodesInKGroup.kt
+++ src/reversenodesinkgroup/ReverseNodesInKGroup.kt
@@ -7,6 +7,50 @@ import utils.nodeListOf
 
 class Solution {
     fun reverseKGroup(head: ListNode?, k: Int): ListNode? {
+        var size = size(head)
+
+        var newHead: ListNode? = null
+        var curHead: ListNode? = head
+        var prevHead: ListNode? = null
+
+        while (k <= size && size != 0) {
+            var prev: ListNode? = null
+            var cur = curHead
+            for (i in (1..k)) {
+                val tmp = cur!!.next
+                cur.next = prev
+
+                prev = cur
+                cur = tmp
+            }
+
+            if (newHead == null) newHead = prev
+            if (prevHead != null) prevHead.next = prev
+            prevHead = curHead
+            curHead = cur
+            size -= k
+        }
+
+        if (k > size || size == 0) {
+            prevHead!!.next = curHead
+        }
+
+        return newHead ?: head
+    }
+
+    fun size(head: ListNode?): Int {
+        var cur = head
+        var result = 0
+        while (cur != null) {
+            cur = cur.next
+            result++
+        }
+        return result
+    }
+}
+
+class SolutionRecursive {
+    fun reverseKGroup(head: ListNode?, k: Int): ListNode? {
         return reverseKGroup(head, k, size(head))
     }
 
@@ -45,7 +89,7 @@ class SolutionTest {
 
     @Test
     fun kLargerThanSize() {
-        Assertions.assertEquals(nodeListOf(1,2,3), s.reverseKGroup(nodeListOf(1,2,3), 4))
+        Assertions.assertEquals(nodeListOf(1, 2, 3), s.reverseKGroup(nodeListOf(1, 2, 3), 4))
     }
 
     @Test
