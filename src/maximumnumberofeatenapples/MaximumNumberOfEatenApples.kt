3a527b8420b46fa3f1fcbb99f6b0c95ec7a739eb
Anton Kapelyushok <anton.kapelyushok@infobip.com>
2021-04-01 18:20:53 +0300

    MaximumNumberOfEatenApples

--- src/maximumnumberofeatenapples/MaximumNumberOfEatenApples.kt
+++ src/maximumnumberofeatenapples/MaximumNumberOfEatenApples.kt
@@ -1,10 +1,54 @@
 package maximumnumberofeatenapples;
 
-import kotlin.test.assertEquals
 import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.test.assertEquals
 
 class Solution {
+    fun eatenApples(apples: IntArray, days: IntArray): Int {
+        val pq = PriorityQueue<Apples>()
 
+        var eaten = 0
+        var day = 0
+        while (pq.isNotEmpty() || day in days.indices) {
+
+            if (day in days.indices && days[day] != 0 && apples[day] != 0) {
+                pq.add(Apples(apples[day], days[day] + day))
+            }
+
+            while (pq.isNotEmpty()) {
+                val next = pq.peek()
+                if (day >= next.consumeBefore) {
+                    pq.poll()
+                } else break
+            }
+
+            val next = pq.peek()
+            if (next == null) {
+                day++
+                continue
+            }
+
+            next.count--
+            eaten++
+            if (next.count == 0) {
+                pq.poll()
+            }
+
+            day++
+        }
+
+        return eaten
+    }
+
+    data class Apples(
+            var count: Int,
+            var consumeBefore: Int
+    ) : Comparable<Apples> {
+        override fun compareTo(other: Apples): Int {
+            return this.consumeBefore.compareTo(other.consumeBefore)
+        }
+    }
 }
 
 class SolutionTest {
@@ -13,7 +57,13 @@ class SolutionTest {
 
     @Test
     fun test1() {
-        assertEquals(0, s.solve())
+        assertEquals(7, s.eatenApples(intArrayOf(1, 2, 3, 5, 2), intArrayOf(3, 2, 1, 4, 2)))
+    }
+
+
+    @Test
+    fun test2() {
+        assertEquals(8, s.eatenApples(intArrayOf(2, 1, 1, 4, 5), intArrayOf(10, 10, 6, 4, 2)))
     }
 
 }
