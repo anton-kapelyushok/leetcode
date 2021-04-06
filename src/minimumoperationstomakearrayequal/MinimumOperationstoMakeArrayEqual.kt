5466b51878e5b7c5d58df3ea257e73162df94c76
Anton Kapelyushok <anton.kapelyushok@infobip.com>
2021-04-06 12:53:39 +0300

    Minimum Operations to Make Array Equal

---
+++ src/minimumoperationstomakearrayequal/MinimumOperationstoMakeArrayEqual.kt
@@ -0,0 +1,9 @@
+package minimumoperationstomakearrayequal;
+
+class Solution {
+    class Solution {
+        fun minOperations(n: Int): Int {
+            return (n * n - n % 2) / 4
+        }
+    }
+}
