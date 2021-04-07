7666c877365b8dd2b552ef814ef25a439b80bf2e
Anton Kapelyushok <anton.kapelyushok@infobip.com>
2021-04-07 17:11:51 +0300

    class Solution {

---
+++ src/longestarithmeticsubsequenceofgivendifference/LongestArithmeticSubsequenceofGivenDifference.kt
@@ -0,0 +1,12 @@
+package longestarithmeticsubsequenceofgivendifference;
+
+class Solution {
+    fun longestSubsequence(arr: IntArray, difference: Int): Int {
+        val map = mutableMapOf<Int, Int>()
+        for (el in arr) {
+            val prev = map[el - difference] ?: 0
+            map[el] = prev + 1
+        }
+        return map.values.max()!!
+    }
+}
