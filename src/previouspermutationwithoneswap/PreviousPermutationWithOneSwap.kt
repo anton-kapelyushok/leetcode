b517f82617523fb0150048141dcbe09ce5d12153
Anton Kapelyushok <anton.kapelyushok@infobip.com>
2021-04-12 23:13:56 +0300

    PreviousPermutationWithOneSwap.kt

--- src/previouspermutationwithoneswap/PreviousPermutationWithOneSwap.kt
+++ src/previouspermutationwithoneswap/PreviousPermutationWithOneSwap.kt
@@ -1,19 +1,28 @@
 package previouspermutationwithoneswap
 
-import kotlin.test.assertEquals
-import org.junit.jupiter.api.Test
-
 class Solution {
+    fun prevPermOpt1(arr: IntArray): IntArray {
+        var l = arr.size
+        for (i in arr.size - 1 downTo 1) {
+            if (arr[i - 1] > arr[i]) {
+                l = i - 1
+                break
+            }
+        }
+        if (l == arr.size) return arr
 
-}
+        var r = l + 1
+        var max = arr[r]
+        var maxR = r++
+        while (r < arr.size) {
+            if (arr[r] > max && arr[r] < arr[l]) {
+                max = arr[r]
+                maxR = r
+            }
+            r++
+        }
 
-class SolutionTest {
-
-    private val s = Solution()
-
-    @Test
-    fun test1() {
-        assertEquals(0, s.solve())
+        arr[l] = arr[maxR].also { arr[maxR] = arr[l] }
+        return arr
     }
-
 }
