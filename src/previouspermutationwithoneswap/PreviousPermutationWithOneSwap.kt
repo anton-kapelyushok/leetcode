e78d29411ea17b79494951765466b3d86f51e051
Anton Kapelyushok <anton.kapelyushok@infobip.com>
2021-04-12 23:13:37 +0300

    PreviousPermutationWithOneSwap.kt

---
+++ src/previouspermutationwithoneswap/PreviousPermutationWithOneSwap.kt
@@ -0,0 +1,19 @@
+package previouspermutationwithoneswap
+
+import kotlin.test.assertEquals
+import org.junit.jupiter.api.Test
+
+class Solution {
+
+}
+
+class SolutionTest {
+
+    private val s = Solution()
+
+    @Test
+    fun test1() {
+        assertEquals(0, s.solve())
+    }
+
+}
