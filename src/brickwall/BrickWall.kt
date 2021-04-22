86eab4d718f94ed71cac060e71aa7c3935552b1f
Anton Kapelyushok <anton.kapelyushok@infobip.com>
2021-04-22 14:21:09 +0300

    Brick Wall

--- src/brickwall/BrickWall.kt
+++ src/brickwall/BrickWall.kt
@@ -1,10 +1,20 @@
 package brickwall
 
-import kotlin.test.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class Solution {
-
+    fun leastBricks(wall: List<List<Int>>): Int {
+        val holes = mutableMapOf<Int, Int>()
+        for (row in wall) {
+            var left = 0
+            for (brickIndex in 0..row.size - 2) {
+                left += row[brickIndex]
+                holes[left] = (holes[left] ?: 0) + 1
+            }
+        }
+        return wall.size - (holes.values.max() ?: 0)
+    }
 }
 
 class SolutionTest {
@@ -13,7 +23,22 @@ class SolutionTest {
 
     @Test
     fun test1() {
-        assertEquals(0, s.solve())
+        assertEquals(2, s.leastBricks(listOf(
+                listOf(1, 2, 2, 1),
+                listOf(3, 1, 2),
+                listOf(1, 3, 2),
+                listOf(2, 4),
+                listOf(3, 1, 2),
+                listOf(1, 3, 1, 1)
+        )))
+    }
+
+    @Test
+    fun test2() {
+        assertEquals(2, s.leastBricks(listOf(
+                listOf(1),
+                listOf(1), listOf(1)
+        )))
     }
 
 }
