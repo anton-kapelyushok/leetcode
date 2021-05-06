24ca75c22b90419d51cb1cbac61af2be7239bb9a
Anton Kapelyushok <anton.kapelyushok@infobip.com>
2021-05-06 18:25:23 +0300

    Convert Sorted List to Binary Search Tree

--- src/convertsortedlisttobinarysearchtree/ConvertSortedListtoBinarySearchTree.kt
+++ src/convertsortedlisttobinarysearchtree/ConvertSortedListtoBinarySearchTree.kt
@@ -1,19 +1,35 @@
 package convertsortedlisttobinarysearchtree
 
-import kotlin.test.assertEquals
-import org.junit.jupiter.api.Test
+import utils.ListNode
+import utils.TreeNode
 
 class Solution {
+    fun sortedListToBST(head: ListNode?): TreeNode? {
+        if (head == null) return null
+        var cur: ListNode? = head
+        val arr = mutableListOf<Int>()
 
-}
+        while (cur != null) {
+            arr.add(cur.`val`)
+            cur = cur.next
+        }
 
-class SolutionTest {
+        arr.sort()
 
-    private val s = Solution()
-
-    @Test
-    fun test1() {
-        assertEquals(0, s.solve())
+        return tree(arr, 0, arr.size - 1)
     }
 
+    fun tree(arr: List<Int>, startIdx: Int, endIdx: Int): TreeNode? {
+        if (startIdx > endIdx) return null
+
+        val mid = (startIdx + endIdx) / 2
+
+        val node = TreeNode(arr[mid])
+        node.left = tree(arr, startIdx, mid - 1)
+        node.right = tree(arr, mid + 1, endIdx)
+
+        return node
+    }
 }
+
+
