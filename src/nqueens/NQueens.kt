814c98d88cc0d66e96e347b5addd3c8df562b2b9
Anton Kapelyushok <anton.kapelyushok@infobip.com>
2021-05-31 14:11:03 +0300

    N-Queens

---
+++ src/nqueens/NQueens.kt
@@ -0,0 +1,19 @@
+package n-queens
+
+import kotlin.test.assertEquals
+import org.junit.jupiter.api.Test
+
+class Solution {
+
+}
+
+class SolutionTest {
+
+    private val s = Solution()
+
+    @Test
+    fun test1() {
+        assertEquals(0, s.solve())
+    }
+
+}
