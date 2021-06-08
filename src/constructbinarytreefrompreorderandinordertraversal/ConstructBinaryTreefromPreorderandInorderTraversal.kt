c054247aa169f85342d5415108c5b7aa17e2df4f
Anton Kapelyushok <anton.kapelyushok@infobip.com>
2021-06-08 12:43:13 +0300

    Construct Binary Tree from Preorder and Inorder Traversal

--- src/constructbinarytreefrompreorderandinordertraversal/ConstructBinaryTreefromPreorderandInorderTraversal.kt
+++ src/constructbinarytreefrompreorderandinordertraversal/ConstructBinaryTreefromPreorderandInorderTraversal.kt
@@ -5,13 +5,15 @@ import utils.TreeNode
 class Solution {
     fun buildTree(preorder: IntArray, inorder: IntArray): TreeNode? {
 
+        val indexOfValue = inorder.withIndex().associate { (i, v) -> v to i }
+
         fun f(pl: Int, pr: Int, il: Int, ir: Int): TreeNode? {
             // println("$pl $pr $il $ir")
             if (pr - pl == 0) return null
 
             val head = TreeNode(preorder[pl])
 
-            val headInOrder = (il until ir).first { inorder[it] == preorder[pl] }
+            val headInOrder = indexOfValue[preorder[pl]]!!
             head.left = f(pl + 1, pl + 1 + headInOrder - il, il, headInOrder)
             head.right = f(pl + headInOrder - il + 1, pr, headInOrder + 1, ir)
 
