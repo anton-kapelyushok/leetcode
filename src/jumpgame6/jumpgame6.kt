41ca9447ff6a86206d5798cdf9f13287cd17ef9e
Anton Kapelyushok <anton.kapelyushok@infobip.com>
2021-06-09 14:52:11 +0300

    Jump Game 6

--- src/jumpgame6/jumpgame6.kt
+++ src/jumpgame6/jumpgame6.kt
@@ -1,10 +1,29 @@
 package jumpgame6
 
-import kotlin.test.assertEquals
 import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.test.assertEquals
 
 class Solution {
+    fun maxResult(nums: IntArray, k: Int): Int {
+        val dp = IntArray(nums.size)
+        val pq = PriorityQueue<Int> { l, r -> dp[r] - dp[l] }
 
+        dp[0] = nums[0]
+
+        pq.offer(0)
+
+        for (i in 1 until nums.size) {
+            while (pq.peek() < i - k) {
+                pq.poll()
+            }
+
+            dp[i] = nums[i] + dp[pq.peek()]
+            pq.offer(i)
+        }
+
+        return dp[nums.size - 1]
+    }
 }
 
 class SolutionTest {
@@ -13,7 +32,12 @@ class SolutionTest {
 
     @Test
     fun test1() {
-        assertEquals(0, s.solve())
+        assertEquals(7, s.maxResult(intArrayOf(1, -1, -2, 4, -7, 3), 2))
+    }
+
+    @Test
+    fun test2() {
+        assertEquals(17, s.maxResult(intArrayOf(10, -5, -2, 4, 0, 3), 3))
     }
 
 }
