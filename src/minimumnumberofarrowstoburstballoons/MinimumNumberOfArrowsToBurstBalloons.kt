db91a87c3bdc155cc550aa97004dbe80995eb658
Anton Kapeliushok <Anton.Kapeliushok@jetbrains.com>
2026-09-16 12:02:02 +0200

    MinimumNumberOfArrowsToBurstBalloons.kt

--- src/minimumnumberofarrowstoburstballoons/MinimumNumberOfArrowsToBurstBalloons.kt
+++ src/minimumnumberofarrowstoburstballoons/MinimumNumberOfArrowsToBurstBalloons.kt
@@ -1,45 +1,39 @@
-package problems
+package minimumnumberofarrowstoburstballoons
 
+import org.junit.jupiter.api.Test
 import java.util.*
+import kotlin.test.assertEquals
 
-object MinimumNumberOfArrowsToBurstBalloons {
-    class Solution {
-        fun findMinArrowShots(points: Array<IntArray>): Int {
-            if (points.isEmpty()) return 0
+class Solution {
+    fun findMinArrowShots(points: Array<IntArray>): Int {
+        if (points.isEmpty()) return 0
 
-            data class Balloon(val l: Int, val r: Int)
+        val pq = PriorityQueue<IntArray>(compareBy { it[1] })
+        points.forEach { pq += it }
 
-            val typedPoints = points.map { (l, r) -> Balloon(l, r) }
+        var lastArrow = pq.peek()[1]
+        var arrowsShot = 1
 
-            val pointsQueue = typedPoints.sortedWith(compareBy({ it.l }, { it.r })).let { LinkedList(it) }
-
-            var res = 0
-            while (pointsQueue.isNotEmpty()) {
-                res += 1
-                var (_, r) = pointsQueue.poll()
-                while (pointsQueue.isNotEmpty()) {
-                    val (newL, newR) = pointsQueue.peek()
-                    if (newL <= r) {
-                        r = minOf(r, newR)
-                        pointsQueue.poll()
-                    } else {
-                        break
-                    }
-                }
+        while (pq.isNotEmpty()) {
+            val b = pq.poll()
+            if (lastArrow in b[0]..b[1]) {
+                continue
             }
-
-            return res
+            lastArrow = b[1]
+            arrowsShot++
         }
+
+        return arrowsShot
+    }
+}
+
+class SolutionTest {
+
+    private val s = Solution()
+
+    @Test
+    fun test1() {
+        assertEquals(0, s.findMinArrowShots(arrayOf()))
     }
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        with(Solution()) {
-            println(
-                findMinArrowShots(
-                    arrayOf(intArrayOf(10, 16), intArrayOf(2, 8), intArrayOf(1, 6), intArrayOf(7, 12))
-                )
-            )
-        }
-    }
 }
